4de4bc47c3650a995f8efaaa44da7737195faeea
nafania2022 <nafania1522@gmail.com>
2023-01-03 16:26:52 +0300

    add flask1

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
-# @app.route('/')
+@app.route('/')
 def hello_world():
     return 'Hello World!'
-
-app.add_url_rule("/", view_func=hello_world)
 
 app.run()
